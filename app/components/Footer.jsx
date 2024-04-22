ef52287ed396a295c63901c1a96faae05f8a0d46
Pranjal Jain <pranjalworkon@gmail.com>
2024-04-23 04:06:17 +0530

    footer links added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
 const Footer = () => {
   return (
 
@@ -10,88 +11,68 @@ const Footer = () => {
   <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
     <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
       <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
+        <Image src="/logo.png" alt="logo" width={50} height={50} />
         <span className="ml-3 text-xl">YuvSrijan</span>
       </a>
-      <p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+      <p className="mt-2 text-sm text-gray-500"> Empowering Tomorrow's Innovators: Bridging the Phygital Gulf</p>
     </div>
     <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Solutions</h2>
         <nav className="list-none mb-10">
           <li>
-            <a className="text-gray-400 hover:text-white">First Link</a>
+            <a className="text-gray-400 hover:text-white">Student Chapter</a>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Second Link</a>
+            <a className="text-gray-400 hover:text-white">Lead</a>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Third Link</a>
+            <a className="text-gray-400 hover:text-white">Mentorship</a>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Fourth Link</a>
+            <a className="text-gray-400 hover:text-white">Community approach</a>
           </li>
         </nav>
       </div>
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Company</h2>
         <nav className="list-none mb-10">
           <li>
-            <a className="text-gray-400 hover:text-white">First Link</a>
+            <Link href="/about" className="text-gray-400 hover:text-white">About us</Link>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Second Link</a>
+            <Link href="/opportunity" className="text-gray-400 hover:text-white">Opportunities</Link>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Third Link</a>
+            <Link href="/partners" className="text-gray-400 hover:text-white">Partners</Link>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Fourth Link</a>
+            <Link href="/contactus" className="text-gray-400 hover:text-white">Contact us</Link>
           </li>
         </nav>
       </div>
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Resources</h2>
         <nav className="list-none mb-10">
           <li>
-            <a className="text-gray-400 hover:text-white">First Link</a>
+            <a className="text-gray-400 hover:text-white">Blog</a>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Second Link</a>
+            <a className="text-gray-400 hover:text-white">Customer reviews</a>
           </li>
           <li>
-            <a className="text-gray-400 hover:text-white">Third Link</a>
+            <a className="text-gray-400 hover:text-white">Affliate Program</a>
           </li>
-          <li>
-            <a className="text-gray-400 hover:text-white">Fourth Link</a>
-          </li>
+
         </nav>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-400 hover:text-white">First Link</a>
-          </li>
-          <li>
-            <a className="text-gray-400 hover:text-white">Second Link</a>
-          </li>
-          <li>
-            <a className="text-gray-400 hover:text-white">Third Link</a>
-          </li>
-          <li>
-            <a className="text-gray-400 hover:text-white">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
+
     </div>
   </div>
   <div className="bg-gray-800 bg-opacity-75">
     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p className="text-gray-400 text-sm text-center sm:text-left">© 2020 Tailblocks —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-500 ml-1" target="_blank">@knyttneve</a>
+      <p className="text-gray-400 text-sm text-center sm:text-left">© 2024 YuvSrijan —
+        <a href="https://twitter.com/" rel="noopener noreferrer" className="text-gray-500 ml-1" target="_blank">@HackHustlers</a>
       </p>
       <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a className="text-gray-400">
