815f207d894c9eb2b101c4b604fe014dfd5446cf
Pranjal Jain <pranjalworkon@gmail.com>
2024-04-23 03:50:59 +0530

    partner added

--- app/partners/page.jsx
+++ app/partners/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">OUR PARTNERS</h1>
-      <p className="lg:w-1/2 w-full leading-relaxed text-opacity-80">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+      <p className="lg:w-1/2 w-full leading-relaxed text-opacity-80">Join our venture and support us in our journey to make an impact .</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -17,8 +17,8 @@ const page = () => {
               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">Shooting Stars</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">Microsoft Azure</h2>
+          <p className="leading-relaxed text-base">Cloud computing platform from Microsoft, known for its integration with other Microsoft products</p>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -30,8 +30,8 @@ const page = () => {
               <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">The Catalyzer</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">Stripe</h2>
+          <p className="leading-relaxed text-base">Payment processing platform for online businesses.</p>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -42,8 +42,9 @@ const page = () => {
               <circle cx="12" cy="7" r="4"></circle>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">Neptune</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">TED Talks</h2>
+          <p className="leading-relaxed text-base">Platform for sharing ideas through short, powerful talks.
+</p>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -53,8 +54,9 @@ const page = () => {
               <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">Melanchole</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">MIT OpenCourseware</h2>
+          <p className="leading-relaxed text-base">Free access to course materials from Massachusetts Institute of Technology.
+</p>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -64,8 +66,9 @@ const page = () => {
               <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">Bunker</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">Hulu</h2>
+          <p className="leading-relaxed text-base">Streaming service with a mix of on-demand and live TV content.
+</p>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
@@ -75,8 +78,9 @@ const page = () => {
               <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
             </svg>
           </div>
-          <h2 className="text-lg text-white font-medium title-font mb-2">Ramona Falls</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-2">PepsiCo</h2>
+          <p className="leading-relaxed text-base">Leading beverage company.
+.</p>
         </div>
       </div>
     </div>
